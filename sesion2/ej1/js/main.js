d7c029bdef2054056e5d456f25e0bfb546e7973e
Cristina Gonzalez <cristina.glez.rubio@gmail.com>
2019-09-03 08:46:39 +0200

    exercise 1 sesion2

--- sesion2/ej1/js/main.js
+++ sesion2/ej1/js/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+class Square {
+    
+    perimeter(long, num){
+        console.log(long * num);
+    }
+    area(num){
+        console.log(num * num);
+    }
+}
+
+const perm = new Square();
+const ar = new Square();
+
+perm.perimeter(9,4);
+ar.area(9);
