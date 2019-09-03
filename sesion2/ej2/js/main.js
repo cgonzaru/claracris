12487b6698ad7073b3ba770b8aa93fe0ee417156
Clara Brígido <clarabrigido@gmail.com>
2019-09-03 09:20:32 +0200

    add update ej2 -m3

--- sesion2/ej2/js/main.js
+++ sesion2/ej2/js/main.js
@@ -13,6 +13,15 @@ class Square {
 }
 
 const newSquare = new Square (9,4);
+const newSquare1= new Square (9,1);
+const newSquare2= new Square (9,3);
+const newSquare3= new Square (9,7);
 
 newSquare.perimeter();
 newSquare.area();
+newSquare1.perimeter();
+newSquare1.area();
+newSquare2.perimeter();
+newSquare2.area();
+newSquare3.perimeter();
+newSquare3.area();
